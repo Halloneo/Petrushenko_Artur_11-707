e0ac025a08fe22eca07ea38925631d0acdc0cf0f
petrushenko.arthur <petrushenko.arthur@outlook.com>
2017-09-29 12:11:01 +0300

    Uploading first part of PS1

---
+++ FALL2017/PS/PS1/PS1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part1_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Решение задачи №15 из 1 части
+            
+            Console.WriteLine("Введите номер билета");
+            int number = int.Parse(Console.ReadLine());
+            int oddSum = 0, evenSum = 0;
+            for (int i = 0; number > 0; i++)
+            {
+                int lastNumber = number % 10;
+                number /= 10;
+                if (i % 2 == 0)
+                    evenSum += lastNumber;
+                else
+                    oddSum += lastNumber;
+            }
+            if (oddSum == evenSum)
+                Console.WriteLine("Является");
+            else
+                Console.WriteLine("Не является");
+            
+        }
+    }
+}
---
+++ FALL2017/PS/PS1/part1_4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part1_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Решение задачи №16 из 1 части
+           
+            Console.WriteLine("Введите номер билета");
+            int number = int.Parse(Console.ReadLine());
+            int firstSum = 0, secondSum = 0;
+            double firstPartOfNumber = 0, secondPartOfNumber = 0;
+            for (int i = 0; number > 0; i++)
+            {
+                int lastNumber = number % 10;
+                number /= 10;
+                if (i < 3)
+                    secondSum += lastNumber;
+                else
+                    firstSum += lastNumber;
+                if (i < 3) // Использую double и деление, чтобы потом не разворачивать число, а просто умножить на 100 и взять целую часть
+                    secondPartOfNumber = secondPartOfNumber / 10 + lastNumber;
+                else
+                    firstPartOfNumber = firstPartOfNumber / 10 + lastNumber;
+            }
+            int firstPart = (int)(firstPartOfNumber * 100);
+            int secondPart = (int)(secondPartOfNumber * 100);
+            if (Math.Abs(firstSum - secondSum) == 1 || Math.Abs(firstPart - secondPart) == 1)
+                Console.WriteLine("Является");
+            else
+                Console.WriteLine("Не является");
+            
+        }
+    }
+}
